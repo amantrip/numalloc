74c3bb4adbb371c48c8581e74895e4fff9382ba8
amantrip <amantrip@cs.utsa.edu>
2015-05-23 22:04:38 -0400

    Updated Routes

--- app/routes.php
+++ app/routes.php
@@ -1,18 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
 
 
-Route::get('/','HomeController@showIndexView');
+//Route::get('/','HomeController@showIndexView');
+Route::get('/', function(){
+   return Redirect::to('/login');
+});
 
 //Authentication Routes
 Route::get('/login', 'HomeController@showLoginView');
